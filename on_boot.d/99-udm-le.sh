573711f2862390156b0b9b1ee27b5e9fbeb5969d
timrettop <musikgoat@gmail.com>
2020-07-23 17:22:00 -0700

    Removed unneeded on_boot portion

--- on_boot.d/99-udm-le.sh
+++ on_boot.d/99-udm-le.sh
@@ -3,15 +3,6 @@
 #Load Environmnt Variables
 . /mnt/data/udm-le/udm-le.env
 
-
-#If flag is set, the following section will re-apply the existing certificate during boot
-
-if [ "$ENABLE_CAPTIVE" == "yes" ]; then
-	podman exec -it unifi-os ${CERT_IMPORT_CMD} ${UNIFIOS_CERT_PATH}/unifi-core.key ${UNIFIOS_CERT_PATH}/unifi-core.crt
-	# This doesn't reboot your router, it just restarts the UnifiOS container
-	unifi-os restart
-fi
-
 if [ ! -f /etc/cron.d/udm-le ]; then
 	# Sleep for 5 minutes to avoid restarting
 	# services during system startup.
